53faf1322212d483119f0e8057be1d773cc4641e
CereuslyDilutedScience <marciakcoburn@gmail.com>
2025-12-25 20:01:12 -0500

    Update BACKEND_URL to deployed Cloud Run URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("script.js updated");
 
 // Set your backend URL here once Cloud Run is deployed
-const BACKEND_URL = "YOUR_BACKEND_URL_HERE";
+const BACKEND_URL = "https://comprehendase-252233072700.us-east4.run.app";
 
 // Enable the button when a file is selected
 document.getElementById("pdf-input").addEventListener("change", () => {
