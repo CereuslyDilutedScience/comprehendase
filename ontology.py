a360356c9ffd4f4584b20fe3815fb03094125ded
CereuslyDilutedScience <marciakcoburn@gmail.com>
2025-12-28 13:48:02 -0500

    Update ontology.py

--- ontology.py
+++ ontology.py
@@ -68,7 +68,7 @@ def lookup_term_ols4(term):
         "q": term,
         "queryFields": "label",
         "fields": "label,description,iri",
-        "exact": "true"
+        "exact": "false"
     }
 
     try:
